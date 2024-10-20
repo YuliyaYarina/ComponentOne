fa591fde65b08a86b9fa26a2db06022b8b3b6f45
Yuliya <Yulechka140200@gmail.com>
2024-10-20 20:00:50 +0500

    доработаны методы

--- src/main/java/org/example/regulator/service/Regulator.java
+++ src/main/java/org/example/regulator/service/Regulator.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface Regulator {
 
-    void setTemperatura(List<Float> temperatura);
+    void setTemperatura(List<Float> outData);
 
     int getValuesTemperature(List<Float> outData);
 
